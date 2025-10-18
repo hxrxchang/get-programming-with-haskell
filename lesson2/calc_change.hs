18b99efad6fe04b9259f90a1c802b66c730a12c4
hxrxchang <mail@hxrxchang.dev>
2025-10-18 17:00:41 +0900

    fix

--- lesson2/calc_change.hs
+++ lesson2/calc_change.hs
@@ -2,7 +2,3 @@ calcChange owed given = if change > 0
                         then change
                         else 0
     where change = given - owed
-
-inc x = x + 1
-double x = x * 2
-square x = x ** 2
