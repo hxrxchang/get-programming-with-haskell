4c83555b1315c964315a37cad90d3def9b7256cc
hxrxchang <mail@hxrxchang.dev>
2025-10-30 00:21:23 +0900

    pattern matching

--- lesson7/main.hs
+++ lesson7/main.hs
@@ -2,3 +2,16 @@ myGCD a b = if remainder == 0
             then b
             else myGCD b remainder
     where remainder = a `mod` b
+
+sayAmount 1 = "one"
+sayAmount 2 = "two"
+sayAmount n = "a bunch"
+
+isEmpty [] = True
+isEmpty _  = False
+
+myHead (x:xs) = x
+myHead [] = error "No head for empty list"
+
+myTail (_:x) = x
+myTail [] = []
