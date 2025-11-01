24e21662dd93d39f57735011b018242055acb67c
hxrxchang <mail@hxrxchang.dev>
2025-11-02 01:32:20 +0900

    lesson9

---
+++ lesson9/main.hs
@@ -0,0 +1,22 @@
+addAnA (x:xs) = ("a " ++ x):addAnA xs
+
+myMap f [] = []
+myMap f (x:xs) = (f x):myMap f xs
+
+
+myFilter test [] = []
+myFilter test (x:xs) = if test x
+                        then x:myFilter test xs
+                        else myFilter test xs
+
+concatAll xs = foldl (++) "" xs
+
+sumOfSquares xs = foldl (+) 0 (map (^2) xs)
+
+myFoldl f init [] = init
+myFoldl f init (x:xs) = myFoldl f newInit xs
+    where newInit = f init x
+
+myFoldr f init [] = init
+myFoldr f init (x:xs) = f x rightResult
+    where rightResult = myFoldr f init xs
